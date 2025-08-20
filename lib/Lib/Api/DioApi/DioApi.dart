ff26a5e564ae08c3ee76962ac87c0582b5260221
MohammadPrince2004 <mostafaprince2008@gmail.com>
2025-08-20 11:21:38 +0300

    Add files via upload

--- lib/Lib/Api/DioApi/DioApi.dart
+++ lib/Lib/Api/DioApi/DioApi.dart
@@ -1,1 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:wheatherapp/Lib/Api/AbsApi/AbsApi.dart';
+import 'package:wheatherapp/Lib/ApiModels/ApiResponse.dart';
 
+class DioApi implements Api {
+  Dio dio = Dio(BaseOptions(baseUrl: "http://api.weatherstack.com/current"));
+
+  @override
+  get(String location) async {
+    ApiResponse apiResponse = ApiResponse();
+    try {
+      Response response = await dio.get(
+        "",
+        queryParameters: {
+          "access_key": "23c447afd978102952f958c28193791d",
+          "query": location,
+        },
+      );
+      // print(response.requestOptions.uri);
+      apiResponse.statusCode = response.statusCode;
+      apiResponse.body = response.data;
+      print(apiResponse.statusCode);
+    } catch (e) {
+      apiResponse.isError = true;
+      apiResponse.msgError = e.toString();
+      print(apiResponse.isError);
+    }
+    print(apiResponse.body);
+    
+    return apiResponse;
+  }
+}
