ef2d971d5df7d32d8357109f143de96d869e440d
MohammadPrince2004 <mostafaprince2008@gmail.com>
2025-08-20 11:27:20 +0300

    Add files via upload

--- lib/Lib/LocationServices/LocationService.dart
+++ lib/Lib/LocationServices/LocationService.dart
@@ -1,1 +1,21 @@
+import 'package:wheatherapp/Lib/GeoLib/GeoLocatorModel.dart';
+import 'package:wheatherapp/Lib/GeoLib/GeoResponse.dart';
 
+class LocationService {
+  GeoLocatorModel geoLocator = GeoLocatorModel();
+  getMyLocation() async {
+    GeoResponse geoResponse = await geoLocator.getLoction();
+    if (geoResponse.isError) {
+      // print(isError);
+      return geoResponse.msgError;
+    }
+    print(geoResponse.subLocality);
+    return {
+      "country": geoResponse.country,
+      "locality": geoResponse.localCity,
+      "sublocality": geoResponse.subLocality,
+      
+
+    };
+  }
+}
